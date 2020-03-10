183fab36f692732d662d0993864dd56e1a4f62e0
(Aaron)Hyunjin Yeo <61129509+jfluke1414@users.noreply.github.com>
2020-03-09 17:50:25 -0700

    Update Cases.php

--- application/controllers/Cases.php
+++ application/controllers/Cases.php
@@ -3,36 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cases extends Crypto_Controller {
 
-	public function __construct()
-	{
+	public function __construct(){
 		parent::__construct();
 		$this->load->helper('download');		
 	}
 	
-	public function index()
-	{		
-		$this->load->view('case_view');	
-		$this->_footer();
+	public function index(){		
+	    $this->load->view('case_view');	
+	    $this->_footer();
 	}
 	
-	
-	public function Accenture_download($filename)
-	{
-		$path = "/home/crypto/assets/download/";
-		$path = $path.$filename;		
-		force_download($path, NULL);
+	public function Accenture_download($filename){
+	    $path = "/home/crypto/assets/download/";
+	    $path = $path.$filename;		
+	    force_download($path, NULL);
 		
-		$msg = "Download completed.";
-		echo("<script>alert('$msg');history.back();</script>");
+	    $msg = "Download completed.";
+	    echo("<script>alert('$msg');history.back();</script>");
 	}	
 	
-	public function pdfviewer($file)
-	{
-		$path = "/assets/download/".$file;
-
-		$data['path'] = $path;
-		$this->load->view('pdf_view', $data);		
-	}
-		
+	public function pdfviewer($file){
+	    $path = "/assets/download/".$file;
+	    $data['path'] = $path;
+	    $this->load->view('pdf_view', $data);		
+	}		
 }
 
