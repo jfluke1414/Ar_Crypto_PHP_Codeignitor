aa78a94671e87602bdc538ddc5eaefbe058535f5
(Aaron)Hyunjin Yeo <61129509+jfluke1414@users.noreply.github.com>
2020-03-09 17:57:05 -0700

    Update Notice.php

--- application/controllers/Notice.php
+++ application/controllers/Notice.php
@@ -3,8 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Notice extends Crypto_Controller {
 
-	public function __construct()
-	{
+	public function __construct(){
 	   parent::__construct();		
 	}
 	
